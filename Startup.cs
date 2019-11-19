f7ff8f7b679ae0f572c15e1b6ad30a39b345a66f
Olivier Lefebvre <aguacongas@gmail.com>
2019-11-19 15:18:25 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -45,29 +45,6 @@ namespace WebApplication1
             {
                 Console.WriteLine($"course code {course.CourseCode}, name {course.CourseName}, subject id {course.Subject.SubjectID} subject name {course.Subject.SubjectName}");
             }
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
         }
     }
 }
